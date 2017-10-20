6a815249330751bd4fae7a9a029d7826882b3c69
BuildTools <unconfigured@null.spigotmc.org>
2017-10-21 07:00:22 +1100

    Commit

--- src/main/java/com/thomasbaum/perimeterplusplus/PerimeterPP.java
+++ src/main/java/com/thomasbaum/perimeterplusplus/PerimeterPP.java
@@ -1,6 +1,5 @@
 package com.thomasbaum.perimeterplusplus;
 
-import com.sun.deploy.util.SessionState;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -8,6 +7,10 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import com.thomasbaum.perimeterplusplus.render.Renderer;
+
+import java.awt.*;
+import java.awt.color.*;
 
 @Mod
 (modid = PerimeterPP.MODID, version = PerimeterPP.VERSION, name = PerimeterPP.NAME)
@@ -22,6 +25,9 @@ public class PerimeterPP {
     public static final String VERSION = "1.0";
     public static final String NAME = "Perimeter Plus Plus";
 
+    @Mod.Instance
+    public static PerimeterPP instance;
+
     @EventHandler
     public void preinit(FMLPreInitializationEvent event){
         System.out.println("Pre-init");
@@ -29,8 +35,14 @@ public class PerimeterPP {
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
+
         System.out.println("Init");
+
+        Renderer.renderBox(1,2,4,6,8,5, Color.pink);
+
     }
+
+
 
     @EventHandler
     public void postinit(FMLPostInitializationEvent event){
--- src/main/java/com/thomasbaum/perimeterplusplus/render/Renderer.java
+++ src/main/java/com/thomasbaum/perimeterplusplus/render/Renderer.java
@@ -1,14 +1,89 @@
 package com.thomasbaum.perimeterplusplus.render;
 
-import java.awt.Color;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
+
 
 public class Renderer {
 
-    public void box(int south_west, int north_east, int xpos, int ypos, int zpos){
+
+
+
+    public static void drawSphere(float cx, float cy, float cz, double rad, int sphereSegments, Color color, float width)
+    {
+        GL11.glColor3f(color.getRed()/255.0F, color.getGreen()/255.0F, color.getBlue()/255.0F);
+        GL11.glPointSize(width);
+
+        final double dPhi = 2*Math.PI / sphereSegments;
+
+        for(double phi = 0.0; phi < 2*Math.PI; phi += dPhi)
+        {
+            double sinPhi = Math.sin(phi);
+            double dTheta = Math.PI / (1 + (int)(sphereSegments * Math.abs(sinPhi/2)));
+            GL11.glBegin(GL11.GL_POINTS);
+            for(double theta = 0.0; theta < Math.PI; theta += dTheta)
+            {
+                float dx = (float)(rad * sinPhi * Math.cos(theta));
+                float dz = (float)(rad * sinPhi * Math.sin(theta));
+                float dy = (float)(rad * Math.cos(phi));
+                GL11.glVertex3f(cx+dx, cy+dy, cz+dz);
+            }
+            GL11.glEnd();
+        }
+    }
+
+    public void renderLine(){
 
     }
 
-    public void sphere(int radius, Color color){
+
+    public static void renderBox(float ax, float ay, float az, float bx, float by, float bz, Color color){
+
+        GL11.glColor3f(color.getRed()/255.0F, color.getGreen()/255.0F, color.getBlue()/255.0F);
+        GL11.glLineWidth(1);
+
+        GL11.glBegin(GL11.GL_LINE_STRIP);
+         GL11.glVertex3f(ax, ay, bz);
+         GL11.glVertex3f(bx, ay, bz);
+         GL11.glVertex3f(bx, ay, az);
+         GL11.glVertex3f(ax, ay, az);
+         GL11.glVertex3f(ax, ay, bz);
+        GL11.glEnd();
+        GL11.glBegin(GL11.GL_LINE_STRIP);
+         GL11.glVertex3f(ax, by, bz);
+         GL11.glVertex3f(bx, by, bz);
+         GL11.glVertex3f(bx, by, az);
+         GL11.glVertex3f(ax, by, az);
+         GL11.glVertex3f(ax, by, bz);
+        GL11.glEnd();
+        GL11.glBegin(GL11.GL_LINES);
+         GL11.glVertex3f(ax, ay, bz);
+         GL11.glVertex3f(ax, by, bz);
+        GL11.glEnd();
+        GL11.glBegin(GL11.GL_LINES);
+         GL11.glVertex3f(bx, ay, bz);
+        GL11.glVertex3f(bx, by, bz);
+        GL11.glEnd();
+        GL11.glBegin(GL11.GL_LINES);
+        GL11.glVertex3f(bx, ay, az);
+        GL11.glVertex3f(bx, by, az);
+        GL11.glEnd();
+        GL11.glBegin(GL11.GL_LINES);
+        GL11.glVertex3f(ax, ay, az);
+        GL11.glVertex3f(ax, by, az);
+        GL11.glEnd();
+
+    }
+
+
+
+    public static void renderSphere(double radius, Color color, double posX, double posY, double posZ){
+
+
+
+
+
 
     }
 
